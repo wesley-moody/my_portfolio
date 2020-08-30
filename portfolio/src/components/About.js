4d735438eb16b9f94326de227b72da1c83df05c4
wesley.moody <wgm3190@gmail.com>
2020-08-30 13:22:10 -0500

    saving about

--- portfolio/src/components/About.js
+++ portfolio/src/components/About.js
@@ -4,7 +4,18 @@ import "../App.css";
 export default function About() {
   return (
     <div className='summary'>
-      <h1>Hello from About!</h1>
+      <h1>Not Your Traditional Developer</h1>
+      <p>
+        My Name is Wesley Moody. I am a full stack web developer with a passion for learing and developiing maintainable, tested code. 
+      </p>
+      <h2>Why Should you HIRE ME?</h2>
+      <ul>
+        <li>Excellent Communication Skills: I've spent the past decade in sales where I was able to craft my </li>
+        <li>
+          
+        </li>
+      </ul>
+
     </div>
   );
 }
